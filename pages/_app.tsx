58a5689b66da4b2d8b9533cfdd87532e29c50aef
Hoan Le <ledaihoan@gmail.com>
2024-07-06 15:46:26 +0700

    feat: add tanstack query support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import "@mantine/core/styles.css";
 import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
+import {
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import { theme } from "../theme";
+import React from "react";
 
 export default function App({ Component, pageProps }: any) {
+  const [queryClient] = React.useState(() => new QueryClient())
   return (
     <MantineProvider theme={theme}>
       <Head>
@@ -14,7 +21,11 @@ export default function App({ Component, pageProps }: any) {
         />
         <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <HydrationBoundary state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </HydrationBoundary>
+      </QueryClientProvider>
     </MantineProvider>
   );
 }
